fbed615c8a167e0e929171820e2972ce4def40b7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-11-26 23:07:57 +0400

    Update for Engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Tramway::SportSchool::Engine.routes.draw do
+  mount Tramway::User::Engine, at: '/users'
+
   root to: 'web/welcome#index'
+
+  scope module: :web do
+    resource :session, only: [ :new, :create, :destroy ]
+  end
 end
