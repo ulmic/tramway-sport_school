51ebd240f698235d0c40b8032d1088e6e5e3225d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-02 02:25:34 +0400

    Add cover

--- app/controllers/tramway/sport_school/web/welcome_controller.rb
+++ app/controllers/tramway/sport_school/web/welcome_controller.rb
@@ -2,5 +2,6 @@ require_dependency "tramway/sport_school/application_controller"
 
 class Tramway::SportSchool::Web::WelcomeController < ApplicationController
   def index
+    @kind_sports = ::Tramway::SportSchool::KindSport.all
   end
 end
--- lib/tramway/sport_school.rb
+++ lib/tramway/sport_school.rb
@@ -3,6 +3,5 @@ require 'tramway/sport_school/generates/install_generator'
 
 module Tramway
   module SportSchool
-    # Your code goes here...
   end
 end
