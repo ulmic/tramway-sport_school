5b5e5e983a3d32a536c5daf7cff67e9ef479364e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-02 02:53:40 +0400

    Add fontawesome

--- lib/tramway/sport_school.rb
+++ lib/tramway/sport_school.rb
@@ -1,5 +1,6 @@
 require "tramway/sport_school/engine"
 require 'tramway/sport_school/generates/install_generator'
+require 'font-awesome-rails'
 
 module Tramway
   module SportSchool
--- lib/tramway/sport_school/engine.rb
+++ lib/tramway/sport_school/engine.rb
@@ -1,3 +1,5 @@
+require 'font-awesome-rails'
+
 module Tramway
   module SportSchool
     class Engine < ::Rails::Engine
