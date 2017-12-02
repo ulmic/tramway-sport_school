a317c38d1ae2de718d39208a22716524a5763cf0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-12-02 15:49:46 +0400

    Add locales

--- lib/tramway/sport_school/engine.rb
+++ lib/tramway/sport_school/engine.rb
@@ -4,6 +4,8 @@ module Tramway
   module SportSchool
     class Engine < ::Rails::Engine
       isolate_namespace Tramway::SportSchool
+      I18n.available_locales = [:ru]
+      I18n.default_locale = :ru
     end
   end
 end
