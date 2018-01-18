cffc1a4f39a56f240a275a860769dc91e4226234
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-01-19 00:03:45 +0400

    Working landing

--- app/controllers/tramway/sport_school/web/welcome_controller.rb
+++ app/controllers/tramway/sport_school/web/welcome_controller.rb
@@ -1,6 +1,8 @@
 require_dependency "tramway/sport_school/application_controller"
 
 class Tramway::SportSchool::Web::WelcomeController < ApplicationController
+  layout 'tramway/sport_school/application'
+
   def index
     @kind_sports = ::Tramway::SportSchool::KindSport.all
   end
