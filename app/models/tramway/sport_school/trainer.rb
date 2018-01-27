e2631ea114fe1b229074e5f1b9995fd742a65afa
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-01-28 02:47:54 +0400

    Updates index pages

--- app/models/tramway/sport_school/trainer.rb
+++ app/models/tramway/sport_school/trainer.rb
@@ -2,7 +2,7 @@ module Tramway::SportSchool
   class Trainer < ::Tramway::Core::ApplicationRecord
     enumerize :degree, in: [ :master_of_sport, :candidate_master_of_sports ]
 
-    mount_uploader :photo, ::Tramway::Core::PhotoUploader
+    mount_uploader :photo, PhotoUploader
 
     state_machine :view_state, initial: :published do
       state :published
