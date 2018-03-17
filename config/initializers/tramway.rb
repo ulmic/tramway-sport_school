ffd0496d3152d9d4645ac598e31a93341835da11
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-03-17 19:34:56 +0400

    Additional buttons in admin panel for social network profiles

--- config/initializers/tramway.rb
+++ config/initializers/tramway.rb
@@ -6,3 +6,19 @@
 ::Tramway::Admin.set_singleton_models ::Tramway::SportSchool::Institution
 ::Tramway::Core.initialize_application model_class: ::Tramway::SportSchool::Institution
 ::Tramway::Profiles.records = [ ::Tramway::SportSchool::Institution ]
+::Tramway::Admin.set_additional_buttons Tramway::SportSchool::Institution.to_s => {
+  show: ::Tramway::Profiles::SocialNetwork.network_name.values.map do |network|
+    {
+      action: :new,
+      model_name: Tramway::Profiles::SocialNetwork.to_s,
+      text: "Добавить профиль #{network}",
+      params: {
+        Tramway::Profiles::SocialNetwork.to_s.underscore => {
+          record_type: -> { model_name.name },
+          record_id: -> { id },
+          network_name: network 
+        }
+      }
+    }
+  end
+}
